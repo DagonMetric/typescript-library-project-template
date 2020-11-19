018f46f396104ae2abbeb69832871c172b7ce1ef
Moe Myat Zaw <mmzliveid@gmail.com>
2020-11-19 12:41:37 +0630

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @license
+ * Copyright copyright-name. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
